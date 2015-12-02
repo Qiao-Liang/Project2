5e4843b7fc33c2b9eb310a78686795a15a44df1a
SlowHacker <innerorouter@gmail.com>
2015-12-02 09:43:50 -0600

    Diagnosis prints added

--- Client.py
+++ Client.py
@@ -23,6 +23,7 @@ objSkt.sendto("GET " + objCP.get("client","file"), (strSrvIP, intSrvPort))
 while True:
 	strResp = objSkt.recv(1040)
 	if strResp == "Completed":
+		print "End looping..."
 		break
 	else:
 		intElm = strResp.index(";")   # There can be many semicolon in the response, but only the first one is the eliminator
@@ -31,14 +32,19 @@ while True:
 		intLen = len(strBit)
 		intSEQ = int(strResp[:intElm])
 
+		print "The lenght of bit received in SEQ %s is %s" % (str(intSEQ), str(intLen))
+
 		dicBuf[intSEQ] = strBit   # Store the packet in buffer, duplicated SEQ is automatically taken care of since the keys in dictionary are unique
 
 		intACK = intSEQ + intLen
 		objSkt.sendto(str(intACK), (strSrvIP, intSrvPort))
+		print "Sent the ACK %s" % (str(intACK))
 
 objFile = open(objCP.get("client", "filedest") + objCP.get("client","file"), 'w')
 
 lstKey = dicBuf.keys()
+print "The number of chunks is %s" % (str(len(dicBuf)))
+
 lstKey.sort()
 for strKey in lstKey:
 	objFile.write(dicBuf[strKey])
